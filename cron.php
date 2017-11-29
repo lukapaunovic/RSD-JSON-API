fcb811b83a6bf078e7bf34b8d27d5fc6043c3949
Luka Paunović <office@leo-host.com>
2017-11-29 13:04:47 +0100

    Update cron.php

--- cron.php
+++ cron.php
@@ -1,3 +1,7 @@
 <?php
 $html = file_get_contents('https://www.nbs.rs/kursnaListaModul/zaDevize.faces?lang=lat');
-file_put_contents('nbs.html', $html);
+if (strpos($html,"zaDevize") == false) {
+        echo "Connection timed out or the site has changed, check for RSD-JSON-API update on github https://github.com/lukapaunovic/RSD-JSON-API";
+        die();
+}
+file_put_contents('/home/hostleo/public_html/come/nbs.html', $html);
