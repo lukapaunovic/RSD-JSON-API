16e111918a6871c3969d0ca1d0767f3fd5db8f9c
Luka Paunović <office@leo-host.com>
2018-03-30 00:09:11 +0200

    Update duo_json.php

--- duo_json.php
+++ duo_json.php
@@ -1,29 +1,26 @@
 <?php
-
 include('simple_html_dom.php');
 $dom = new DomDocument;
 $dom->loadHtmlFile('nbs.html');
-
 $xpath = new DomXPath($dom);
 
 // collect header names
 $headerNames = array();
 foreach ($xpath->query('//table[@id="index:spisakDeviza"]//th') as $node) {
-    $headerNames[] = $node->nodeValue;
+	$headerNames[] = $node->nodeValue;
 }
 
 // collect data
 $data = array();
 foreach ($xpath->query('//tbody[@id="index:spisakDeviz:tbody_element"]/tr') as $node) {
-    $rowData = array();
-    foreach ($xpath->query('td', $node) as $cell) {
-        $rowData[] = $cell->nodeValue;
-    }
-
-    $data[] = array_combine($headerNames, $rowData);
+	$rowData = array();
+	foreach ($xpath->query('td', $node) as $cell) {
+		$rowData[] = $cell->nodeValue;
+	}
+	$data[] = array_combine($headerNames, $rowData);
 }
-if (is_array($data) || is_object($data)) {
 
+if (is_array($data) || is_object($data)) {
 	$result = array();
 	foreach($data as $row) { 
 		$arr = array("code"=>$row['OZNAKA VALUTE'], "name"=>$row['NAZIV ZEMLJE'], "rate"=>$row['PRODAJNI KURS']);
@@ -31,5 +28,4 @@ if (is_array($data) || is_object($data)) {
 	}
 	header('Content-Type: application/json');
 	echo json_encode($result, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-
-} 
+}
